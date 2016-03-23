6e23c509321c51cc338d4226c952b2c88252628f
Valentina <chrnoeva@magora.systems>
2016-03-23 15:44:55 +0600

    create project

---
+++ UserProfile/UserProfile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UserProfile
+//
+//  Created by Valentina Chernoeva on 23.03.16.
+//  Copyright © 2016 Valentina Chernoeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
