04dffab894d5ef160605cb50d1d86aad4519eee1
Valentina <chrnoeva@magora.systems>
2016-03-24 16:26:28 +0600

    create Home module

---
+++ UserProfile/UserProfile/Modules/Home/BuisnessLogic/HomeInteractor.swift
@@ -0,0 +1,13 @@
+//
+//  HomeInteractor.swift
+//  UserProfile
+//
+//  Created by Valentina Chernoeva on 24.03.16.
+//  Copyright © 2016 Valentina Chernoeva. All rights reserved.
+//
+
+import UIKit
+
+class HomeInteractor: NSObject, HomeInteractorProtocol {
+    
+}
---
+++ UserProfile/UserProfile/Modules/Home/UI/HomeVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVC.swift
+//  UserProfile
+//
+//  Created by Valentina Chernoeva on 24.03.16.
+//  Copyright © 2016 Valentina Chernoeva. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
