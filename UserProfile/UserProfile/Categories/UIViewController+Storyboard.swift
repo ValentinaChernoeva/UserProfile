b2f343dd045b43194aba8eb212e801310dd4c79f
Valentina <chrnoeva@magora.systems>
2016-03-28 17:09:12 +0600

    add UIVewController category

---
+++ UserProfile/UserProfile/Categories/UIViewController+Storyboard.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewController+Storyboard.swift
+//  LoginTest
+//
+//  Created by Valentina Chernoeva on 28.03.16.
+//  Copyright © 2016 Константин. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let storyboardNameHome = "Home"
+    static let storyboardNameProfile = "UserProfile"
+}
+
+extension UIViewController {
+    
+    class func instantiateFromStoryboard(storyboardName: String) -> Self {
+        return instantiateFromStoryboardHelper(storyboardName)
+    }
+    
+    class private func instantiateFromStoryboardHelper<T>(storyboardName: String) -> T {
+            let storyboard = UIStoryboard(name: storyboardName, bundle: nil)
+            return storyboard.instantiateViewControllerWithIdentifier(storyboardIdentifier()) as! T
+    }
+    
+    class private func storyboardIdentifier() -> String {
+        return String(self)
+    }
+}
